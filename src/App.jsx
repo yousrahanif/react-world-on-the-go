b89157eef8581dbd97b8ab846141978c049d6e74
yousrahanif <yousrahanif26@gmail.com>
2026-02-25 22:27:25 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Countries from "./components/Countries/Countries";
+
+function App() {
+  return (
+    <>
+      <Countries></Countries>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Countries/Countries.jsx
@@ -0,0 +1,9 @@
+const Countries = () => {
+  return (
+    <div>
+      <h1>Countries</h1>
+    </div>
+  );
+};
+
+export default Countries;
